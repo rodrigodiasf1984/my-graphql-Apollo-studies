046eeb15f1c84b66f0048006b2da0d52ab1be07a
Rodrigo <rodrigodiasf@gmail.com>
2020-05-22 19:42:03 +0100

    adicionada query para recuperar um book

--- books-app-ninja/server/schema/schema.js
+++ books-app-ninja/server/schema/schema.js
@@ -1,18 +1,18 @@
 const graphql = require('graphql');
 const _= require('lodash');
 
-const{GraphQLObjectType, GraphQLString, GraphQLSchema}= graphql;
+const{GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID}= graphql;
 
 var books = [
   {name: 'Planet of Apes', genre: 'Fiction', id:'1'},
   {name: 'Foundation', genre: 'Fiction', id:'2'},
-  {name: 'The Silmarillion', genre: 'Fantasy', id:'1'},
+  {name: 'The Silmarillion', genre: 'Fantasy', id:'3'},
 ]
 
 const BookType = new GraphQLObjectType({
   name:'Book',
   fields:() => ({
-    id: {type:GraphQLString},
+    id: {type:GraphQLID},
     name:{type:GraphQLString},
     genre:{type:GraphQLString}
   })
@@ -23,9 +23,10 @@ const RootQuery = new GraphQLObjectType({
   fields:{
     book:{
       type:BookType,
-      args:{id: {type:GraphQLString}},
+      args:{id: {type:GraphQLID}},
       resolve(parent, args){
         // code to get data from db / other source
+        console.log(typeof(args.id))
         return _.find(books,{id:args.id});
       }
     }
